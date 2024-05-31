d03c150e8b12bb7a89701a2b0e3603334175d076
Minta-Darkwah <ohenebaminta.darkwah@istitutoagnelli.it>
2024-05-31 09:00:14 +0200

    modifica sito

--- src/lib/components/pulsante.svelte
+++ src/lib/components/pulsante.svelte
@@ -1,75 +1,178 @@
-<script>
-    export let src;
-    export let nome;
-</script>
+<link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+/>
 
-<div id="pulsante">
-    <a href={src}>
-        <div id="sin">{nome}</div>
-        <span class="material-symbols-outlined">navigate_next</span>
-    </a>
-    
+<div id="nav_b">
+    <div class="bot">
+        <a href="/pag_iniz">
+            <span class="material-symbols-outlined">home</span>
+        </a>
+    </div>
+    <div class="bot">
+        <a href="/pag_1945"> 1945 </a>
+    </div>
+    <div class="bot">
+        <a href="/pag_2020"> 2020 </a>
+    </div>
+    <div class="bot">
+        <a href="/pag_mafia"> MAFIA </a>
+    </div>
+    <div class="bot">
+        <a href="/pag_borsellino"> Borsellino </a>
+    </div>
+    <div class="bot">
+        <a href="/pag_penadimorte"> Pena di morte </a>
+    </div>
+    <div class="bot">
+        <a href="/pag_femminismo"> Femminismo </a>
+    </div>
+    <div class="bot">
+        <a href="/pag_dittature"> Dittature </a>
+    </div>
 </div>
 
 <style>
-    #pulsante
-    {
-        width: 15%;
-        height: 3rem;
+    #nav_b {
+        width: 10rem;
+        /* height: 18rem; */
 
         display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
+        flex-direction: column;
 
-        font-size: 1.3rem;
+        position: fixed;
+        top: 30%;
+        left: 1rem;
 
-        border-radius: 0.2rem;
+        float: right;
 
-        background-color: #0D1B2A;
-        color: #CAF0F8;
-
-        margin-bottom: 3rem;
-        margin-left: 85%;
-
-        border: 2px solid #E0E1DD;
-
-
+        border: 1px solid black;
     }
-    a
-    {
+    .material-symbols-outlined {
+        width: 5%;
+
+        font-size: 1.5pc;
+
+        padding-left: 2%;
+        padding-top: 0.7%;
+
+        border: 1px solid black;
+    }
+    #nav_b span {
+        width: 17%;
+        height: 35%;
+    }
+    a {
         width: 100%;
         height: 100%;
 
         display: flex;
-        flex-direction: row;
+        justify-content: center;
+
+        font-size: 1.5rem;
+        color: grey;
+        text-decoration: none;
+    }
+    #nav_b div {
+        width: 100%;
+        height: 100%;
+
+        display: flex;
         justify-content: center;
         align-items: center;
 
-        color: #E0E1DD;
-        text-decoration: none;
-        
-    }
-    a:hover
-    {
-        
-        color: #0D1B2A;
-    }
-    #pulsante:hover
-    {
-        background-color: #E0E1DD;
-        color: #0D1B2A;
-        border: 2px solid #0D1B2A;
-    }
-    span
-    {
+        row-gap: 3rem;
 
-        margin-left: 0.5rem;
+        border-radius: 0.5rem;
+
+        border: 2px solid #778da9;
     }
-   
-    .material-symbols-outlined
-    {
-        width: 12%;
-        height: 45%;
+    #nav_b div:hover{
+        background-color: #415A77;
+        
+    }
+    a {
+        color: #E0E1DD;
+    }
+
+    .bot {
+        min-height: 2.88rem;
+
+        margin: 0.2rem 0rem 0rem 0rem;
+        /**/
+        font-family: inherit;
+        z-index: 0;
+        overflow: hidden;
+        transition: all 0.3s cubic-bezier(0.02, 0.01, 0.47, 1);
+
+        background-color: #0d1b2a;
+
+        /* border: 3px solid orange; */
+    }
+
+    /**/
+    .bot:hover {
+        animation: sh0 0.5s ease-in-out both;
+    }
+
+    @keyframes sh0 {
+        0% {
+            transform: rotate(0deg) translate3d(0, 0, 0);
+        }
+
+        25% {
+            transform: rotate(7deg) translate3d(0, 0, 0);
+        }
+
+        50% {
+            transform: rotate(-7deg) translate3d(0, 0, 0);
+        }
+
+        75% {
+            transform: rotate(1deg) translate3d(0, 0, 0);
+        }
+
+        100% {
+            transform: rotate(0deg) translate3d(0, 0, 0);
+        }
+    }
+
+    .bot:hover span {
+        animation: storm 0.7s ease-in-out both;
+        animation-delay: 0.06s;
+    }
+
+    .bot::before,
+    .bot::after {
+        content: "";
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+        background: #fff;
+        opacity: 0;
+        transition:
+            transform 0.15s cubic-bezier(0.02, 0.01, 0.47, 1),
+            opacity 0.15s cubic-bezier(0.02, 0.01, 0.47, 1);
+        z-index: -1;
+        transform: translate(100%, -25%) translate3d(0, 0, 0);
+    }
+
+    .bot:hover::before,
+    .bot:hover::after {
+        opacity: 0.15;
+        transition:
+            transform 0.2s cubic-bezier(0.02, 0.01, 0.47, 1),
+            opacity 0.2s cubic-bezier(0.02, 0.01, 0.47, 1);
+    }
+
+    .bot:hover::before {
+        transform: translate3d(50%, 0, 0) scale(0.9);
+    }
+
+    .bot:hover::after {
+        transform: translate(50%, 0) scale(1.1);
     }
 </style>
